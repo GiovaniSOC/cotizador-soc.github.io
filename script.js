058fd00d21239587a57066592c4508776daf616e
GiovaniSOC <115258247+GiovaniSOC@users.noreply.github.com>
2022-11-07 11:30:59 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -20,3 +20,19 @@ item.addEventListener("click", () => {
         }
 });
 })
+
+btn = document.getElementById('seleccionar-todos');
+btn.addEventListener("click", () => {
+    items.forEach(item => {
+        item.classList.add("checked");
+
+    let checked = document.querySelectorAll(".checked"),
+        btnText = document.querySelector(".btn-text");
+
+        if(checked && checked.length > 0){
+            btnText.innerText = `${checked.length} Seleccionado(s)`
+        }else{
+            btnText.innerText = "Seleccionados";
+        }
+    });
+});
